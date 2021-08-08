dd2240ff613ace3a61cb58b0adc7680acc7c5807
David <43989606+Davidcep@users.noreply.github.com>
2021-08-08 22:37:59 +0200

    Update OompaDao.kt

--- app/src/main/java/com/example/ompaaloompa/data/local/OompaDao.kt
+++ app/src/main/java/com/example/ompaaloompa/data/local/OompaDao.kt
@@ -13,12 +13,12 @@ interface OompaDao {
     @Query("SELECT * FROM oompas")
     fun getAllOompas() : LiveData<List<Oompa>>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertAll(characters: List<Oompa>)
-
     @Query("SELECT * FROM oompas WHERE first_name LIKE :name")
     fun getOompaByName(name: String): LiveData<List<Oompa>>
 
     @Query("SELECT * FROM oompas WHERE id = :id")
     fun getOompaById(id: Int): LiveData<Oompa>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(characters: List<Oompa>)
 }
