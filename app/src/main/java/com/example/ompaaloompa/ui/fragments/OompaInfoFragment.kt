4363388bacafd5167fb041a168dd9cbf56237058
David <43989606+Davidcep@users.noreply.github.com>
2021-08-09 19:35:29 +0200

    Update OompaInfoFragment.kt

--- app/src/main/java/com/example/ompaaloompa/ui/fragments/OompaInfoFragment.kt
+++ app/src/main/java/com/example/ompaaloompa/ui/fragments/OompaInfoFragment.kt
@@ -5,29 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.ompaaloompa.R
+import com.example.ompaaloompa.databinding.FragmentOompaInfoBinding
+import com.example.ompaaloompa.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class OompaInfoFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = OompaInfoFragment()
-    }
-
     private lateinit var viewModel: OompaInfoViewModel
+    private lateinit var binding: FragmentOompaInfoBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_oompa_info, container, false)
+        binding = FragmentOompaInfoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(OompaInfoViewModel::class.java)
-        val idOompa = arguments?.let{ requireArguments().getInt("id") }
+        val id = arguments?.let{ requireArguments().getInt("id") }
+        if(id != null) bindData(id)
+    }
+
+    private fun bindData(id: Int){
+        viewModel.searchOompaById(id).observe(viewLifecycleOwner, Observer {
+            with(binding){
+                tvInfoName.text = it.first_name
+                tvInfoLastName.text = it.last_name
+                tvInfoProfession.text = it.profession
+                tvInfoGender.text = it.gender
+                executePendingBindings()
+            }
+        })
     }
 }
